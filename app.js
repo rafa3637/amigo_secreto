ddbe82e9216bbbd1b9df322d32d0a058d7863e67
rafa3637 <rafaoliverpi@gmail.com>
2025-01-18 18:15:42 -0300

    feita alteração na função sortearAmigo e criado a função reiniciarJogo

--- app.js
+++ app.js
@@ -11,32 +11,47 @@ function adicionarAmigo() {
         amigos.push(nomeAmigo);
     }
     
+    document.getElementById("amigo").value = "";
     atualizarListaAmigos();
-
-    document.getElementById("nomeAmigo").value = "";
 }
 
 function exibirListaAmigos() {
-    const listaAmigos = document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";  // Limpar a lista existente
 
     for (let i = 0; i < amigos.length; i++) {
-        const item = document.createElement("li"); // Criar um novo elemento de lista (<li>) para cada nome
-        item.textContent = amigos[i];
-        listaAmigos.appendChild(item); // Adicionar o elemento à lista
+       listaAmigos.innerHTML += "<li>" + amigos[i] + "</li>"
     }    
 }
 
 function atualizarListaAmigos() {
-    const lista =  getElementById("listaAmigos");
+    const lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
 
      for (let i = 0; i < amigos.length; i++) {
-        listaAmigos.innerHTML += "<li>" + amigos[i] + "<li>" // Adicionar cada amigo como um item da lista
+        listaAmigos.innerHTML += "<li>" + amigos[i] + "</li>" // Adicionar cada amigo como um item da lista     
     }
 }
 
- 
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Alista está vazia. Adicione amigos antes de sortear.");
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<li>Meu amigo secreto é: " + amigoSorteado + "<li>";
+        amigos = []; //limpa o array de amigos
+        atualizarListaAmigos(); //atualiza a lista para não mostrar mais nada
+    }
+}
 
+function reiniciarJogo() {
+    amigos = []; // Limpa o array de amigos
+    document.getElementById("listaAmigos").innerHTML = ""; //limpa a lista de nomes exibida
+    document.getElementById("resultado").innerHTML = ""; //limpa o resultado do sorteio
+    document.getElementById("amigo").value = ""; //limpa o campo de entrada
+    alert("O jogo foi reiniciado!");
+}
 
 
